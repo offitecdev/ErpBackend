56fd217e48918e36b1f47aed3d5e259b54b4eca8
Samet <sametrabia2003@gmail.com>
2026-06-02 10:21:11 +0300

    yenicommit

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
+const Module = require('module');
 const { execFileSync } = require('child_process');
 
 const distEntry = path.join(__dirname, 'dist', 'main.js');
+
+function registerNodePaths() {
+    const paths = [];
+    const homeDir = process.env.HOME || os.homedir();
+    const nodeMajor = process.versions.node.split('.')[0];
+    const publicHtmlDir = path.join(homeDir, 'public_html');
+    const appRelativePath = path.relative(publicHtmlDir, __dirname);
+
+    if (process.env.VIRTUAL_ENV) {
+        paths.push(path.join(process.env.VIRTUAL_ENV, 'lib', 'node_modules'));
+    }
+
+    if (appRelativePath && !appRelativePath.startsWith('..') && !path.isAbsolute(appRelativePath)) {
+        paths.push(path.join(homeDir, 'nodevenv', 'public_html', appRelativePath, nodeMajor, 'lib', 'node_modules'));
+    }
+
+    paths.push(path.join(__dirname, 'node_modules'));
+
+    const currentNodePath = process.env.NODE_PATH ? process.env.NODE_PATH.split(path.delimiter) : [];
+    const nextNodePath = [...new Set([...paths.filter((item) => fs.existsSync(item)), ...currentNodePath])];
+
+    if (nextNodePath.length) {
+        process.env.NODE_PATH = nextNodePath.join(path.delimiter);
+        Module._initPaths();
+    }
+}
 
 function ensurePrismaClient() {
     try {
         require('@prisma/client/default');
     } catch (error) {
         const message = error && error.message ? error.message : '';
-        if (!message.includes('.prisma/client')) {
+        if (!message.includes('.prisma/client') && !message.includes('@prisma/client')) {
             throw error;
         }
 
@@ -23,6 +51,7 @@ function ensurePrismaClient() {
     }
 }
 
+registerNodePaths();
 ensurePrismaClient();
 
 if (fs.existsSync(distEntry)) {
